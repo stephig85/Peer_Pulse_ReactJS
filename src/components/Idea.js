871cb8992a162bbd97c0a8cf426eda83c5e1c62e
Stephanie <stephig85@gmail.com>
2017-01-26 13:10:43 -0600

    Update Idea.js

--- src/components/Idea.js
+++ src/components/Idea.js
@@ -4,19 +4,13 @@ import Votes from './Votes';
 import { Col } from 'react-bootstrap';
 
 class Idea extends Component {
-
-
-
   render() {
-
     return (
-      <div id="topicsDiv">
-
+      <div className="topicsDiv">
       <Col xs={9} className="ideaName">
           <h4 className="idea-title-header">{this.props.ideaObject}</h4>
-    </Col>
-
-    </div>
+      </Col>
+      </div>
     );
   }
 }
